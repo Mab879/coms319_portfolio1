9f022ad6ddf48a9c41fea5dee81ec52ccc729495
Matthew Burket <mb@matthewburket.com>
2016-02-07 15:03:32 -0600

    Added a click button for zipcode worked on starting trends

--- SwingingWeather/src/swingingWeather/SwingWeatherMain.java
+++ SwingingWeather/src/swingingWeather/SwingWeatherMain.java
@@ -3,16 +3,18 @@ package swingingWeather;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
-import java.awt.GridLayout;
 import javax.swing.JLabel;
-import java.awt.FlowLayout;
-import java.awt.BorderLayout;
 import javax.swing.JPanel;
 import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.util.regex.Pattern;
+import javax.swing.JOptionPane;
 
 public class SwingWeatherMain {
 
 	private JFrame frame;
+	private JTextField zipCodeField;
 
 	/**
 	 * Launch the application.
@@ -47,7 +49,7 @@ public class SwingWeatherMain {
 		frame.getContentPane().setLayout(null);
 		
 		JPanel currentPanel = new JPanel();
-		currentPanel.setBounds(0, 0, 292, 108);
+		currentPanel.setBounds(0, 0, 225, 61);
 		frame.getContentPane().add(currentPanel);
 		currentPanel.setLayout(null);
 		
@@ -63,5 +65,76 @@ public class SwingWeatherMain {
 		JLabel lblNewLabel = new JLabel("Current");
 		lblNewLabel.setBounds(20, 6, 115, 15);
 		currentPanel.add(lblNewLabel);
+		
+		JLabel currentWindLabel = new JLabel("W");
+		currentWindLabel.setBounds(96, 6, 70, 15);
+		currentPanel.add(currentWindLabel);
+		
+		JLabel currentWindSpeedValue = new JLabel("60");
+		currentWindSpeedValue.setBounds(125, 6, 70, 15);
+		currentPanel.add(currentWindSpeedValue);
+		
+		JLabel currentWindUnits = new JLabel("MPH");
+		currentWindUnits.setBounds(147, 6, 70, 15);
+		currentPanel.add(currentWindUnits);
+		
+		JLabel currentHumidyLabel = new JLabel("H");
+		currentHumidyLabel.setBounds(96, 20, 70, 15);
+		currentPanel.add(currentHumidyLabel);
+		
+		JLabel currentHumdityValue = new JLabel("42");
+		currentHumdityValue.setBounds(125, 20, 70, 15);
+		currentPanel.add(currentHumdityValue);
+		
+		JLabel currentHumidtyLabel = new JLabel("%");
+		currentHumidtyLabel.setBounds(147, 21, 70, 15);
+		currentPanel.add(currentHumidtyLabel);
+		
+		JLabel currentPressureLabel = new JLabel("P");
+		currentPressureLabel.setBounds(96, 33, 70, 15);
+		currentPanel.add(currentPressureLabel);
+		
+		JLabel currentPressureValue = new JLabel("29.92");
+		currentPressureValue.setBounds(125, 31, 70, 15);
+		currentPanel.add(currentPressureValue);
+		
+		JLabel currentPressureUnit = new JLabel("mb");
+		currentPressureUnit.setBounds(168, 31, 70, 15);
+		currentPanel.add(currentPressureUnit);
+		
+		JLabel currentObservation = new JLabel("Clouds: few clouds");
+		currentObservation.setBounds(20, 48, 176, 15);
+		currentPanel.add(currentObservation);
+		
+		JLabel zipCodeLabel = new JLabel("Zip Code:");
+		zipCodeLabel.setBounds(12, 341, 70, 15);
+		frame.getContentPane().add(zipCodeLabel);
+		
+		zipCodeField = new JTextField(5);
+		zipCodeField.setBounds(94, 339, 114, 19);
+		frame.getContentPane().add(zipCodeField);
+		zipCodeField.setColumns(5);
+		
+		JButton btnGo = new JButton("GO");
+		btnGo.addActionListener(e -> {
+			String zipPattern = "\\d\\d\\d\\d\\d";
+			if (!Pattern.matches(zipPattern, zipCodeField.getText())) {
+				JOptionPane.showMessageDialog(null, "Invalid zip code.", "Error", JOptionPane.ERROR_MESSAGE);		
+			}
+		});
+		
+		btnGo.setBounds(219, 336, 70, 25);
+		frame.getContentPane().add(btnGo);
+		
+		JPanel trendsWrapper = new JPanel();
+		trendsWrapper.setBounds(237, 27, 311, 182);
+		frame.getContentPane().add(trendsWrapper);
+		
+	
+		
+		JLabel lblTrends = new JLabel("Trends");
+		lblTrends.setBounds(269, 12, 70, 15);
+		frame.getContentPane().add(lblTrends);
+	
 	}
 }
