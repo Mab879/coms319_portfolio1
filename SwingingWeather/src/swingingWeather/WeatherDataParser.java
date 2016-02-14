071a2a2e08ea6eadebfb6e0eb9c2422787ae56d5
David Garner <appdude303@gmail.com>
2016-02-14 12:52:30 -0600

    Added WeatherDataViewController to bind the Data from the model to the View

--- SwingingWeather/src/swingingWeather/WeatherDataParser.java
+++ SwingingWeather/src/swingingWeather/WeatherDataParser.java
@@ -118,7 +118,6 @@ public class WeatherDataParser {
 
 		parsedData.add(weatherData);
 		
-		
 		}
 	}
 }
---
+++ SwingingWeather/src/swingingWeather/WeatherViewController.java
@@ -0,0 +1,10 @@
+package swingingWeather;
+
+public class WeatherViewController {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
