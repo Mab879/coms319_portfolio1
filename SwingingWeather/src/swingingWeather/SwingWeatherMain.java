587a9b03fe06b626e805b6ff02f6f4a2791cfa31
Matthew Burket <mb@matthewburket.com>
2016-02-15 15:56:53 -0600

    Docs and formatting

--- SwingingWeather/src/swingingWeather/SwingWeatherMain.java
+++ SwingingWeather/src/swingingWeather/SwingWeatherMain.java
@@ -7,108 +7,134 @@ import java.awt.Font;
 import javafx.embed.swing.JFXPanel;
 import javafx.application.Platform;
 
+/**
+ * The main window for the applicaiton
+ * 
+ * @author Matthew Burket and David Garner
+ *
+ */
 public class SwingWeatherMain {
-
+	
+	/**
+	 * The main frame of the application
+	 */
 	private JFrame frame;
+	
+	/**
+	 * The text field the zip code is entered
+	 */
 	private JTextField zipCodeField;
-	private JLabel currentTempValue,currentWindLabel,currentWindSpeedValue,currentWindUnits,
-	currentHumdityValue,currentPressureValue,currentObservation;
+	/**
+	 * The many labels that data is displayd in
+	 */
+	private JLabel currentTempValue, currentWindLabel, currentWindSpeedValue, currentWindUnits, currentHumdityValue,
+			currentPressureValue, currentObservation;
+	/**
+	 * The go button that is pressed to update the data
+	 */
 	private JButton btnGo;
+	
+	/**
+	 * An array of the forecast panels for the forecast section
+	 */
 	private ForecastPanel[] forecasts;
+	
+	/**
+	 * The trend chart for highs and lows
+	 */
 	public TrendChart chart = new TrendChart();
 	private final int forecastDays = 5;
+
 	/**
-	 * Create the application.
+	 * Create the application window
 	 */
 	public SwingWeatherMain() {
 		initialize();
-		
+
 	}
 
 	/**
 	 * Initialize the contents of the frame.
-	 * @return 
 	 */
 	public void initialize() {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 640, 495);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-        frame.setTitle("SwingingWeather");
-		
+		frame.setTitle("SwingingWeather");
+
 		JPanel currentPanel = new JPanel();
 		currentPanel.setBounds(0, 0, 225, 61);
 		frame.getContentPane().add(currentPanel);
 		currentPanel.setLayout(null);
-		
+
 		currentTempValue = new JLabel("104", SwingConstants.RIGHT);
 		currentTempValue.setFont(new Font("DejaVu Sans", Font.PLAIN, 24));
 		currentTempValue.setBounds(8, 20, 59, 29);
 		currentPanel.add(currentTempValue);
-		
+
 		JLabel lblF = new JLabel("°F");
 		lblF.setBounds(65, 21, 22, 15);
 		currentPanel.add(lblF);
-		
+
 		JLabel lblNewLabel = new JLabel("Current");
 		lblNewLabel.setBounds(20, 6, 115, 15);
 		currentPanel.add(lblNewLabel);
-		
+
 		currentWindLabel = new JLabel("W");
 		currentWindLabel.setBounds(96, 6, 17, 15);
 		currentPanel.add(currentWindLabel);
-		
+
 		currentWindSpeedValue = new JLabel("60", SwingConstants.RIGHT);
 		currentWindSpeedValue.setBounds(65, 6, 90, 15);
 		currentPanel.add(currentWindSpeedValue);
-		
+
 		currentWindUnits = new JLabel("MPH");
 		currentWindUnits.setBounds(157, 6, 39, 15);
 		currentPanel.add(currentWindUnits);
-		
+
 		JLabel currentHumidyLabel = new JLabel("H");
-		currentHumidyLabel.setBounds(96 , 20, 17, 15);
+		currentHumidyLabel.setBounds(96, 20, 17, 15);
 		currentPanel.add(currentHumidyLabel);
-		
+
 		currentHumdityValue = new JLabel("42.0", SwingConstants.RIGHT);
 		currentHumdityValue.setBounds(106, 20, 59, 15);
 		currentPanel.add(currentHumdityValue);
-		
+
 		JLabel currentHumidtyLabel = new JLabel("%");
 		currentHumidtyLabel.setBounds(167, 21, 17, 15);
 		currentPanel.add(currentHumidtyLabel);
-		
+
 		JLabel currentPressureLabel = new JLabel("P");
 		currentPressureLabel.setBounds(96, 33, 17, 15);
 		currentPanel.add(currentPressureLabel);
-		
+
 		currentPressureValue = new JLabel("29.92", SwingConstants.RIGHT);
 		currentPressureValue.setBounds(106, 31, 49, 15);
 		currentPanel.add(currentPressureValue);
-		
+
 		JLabel currentPressureUnit = new JLabel("mb");
 		currentPressureUnit.setBounds(157, 31, 39, 15);
 		currentPanel.add(currentPressureUnit);
-		
+
 		currentObservation = new JLabel("Clouds: few clouds");
 		currentObservation.setBounds(20, 48, 176, 15);
 		currentPanel.add(currentObservation);
-		
+
 		JLabel zipCodeLabel = new JLabel("Zip Code:");
 		zipCodeLabel.setBounds(6, 447, 70, 15);
 		frame.getContentPane().add(zipCodeLabel);
-		
+
 		zipCodeField = new JTextField(5);
 		zipCodeField.setBounds(88, 445, 114, 19);
 		frame.getContentPane().add(zipCodeField);
 		zipCodeField.setColumns(5);
-		
+
 		btnGo = new JButton("GO");
-		
-		
+
 		btnGo.setBounds(213, 442, 70, 25);
 		frame.getContentPane().add(btnGo);
-		
+
 		JPanel trendsWrapper = new JPanel();
 		trendsWrapper.setBounds(308, 21, 311, 190);
 		frame.getContentPane().add(trendsWrapper);
@@ -122,113 +148,129 @@ public class SwingWeatherMain {
 				chart.initFX(jfxPanel);
 			}
 		});
-		
+
 		JLabel lblTrends = new JLabel("Trends");
 		lblTrends.setBounds(308, 6, 70, 15);
 		frame.getContentPane().add(lblTrends);
-		
+
 		JLabel lblDayFor = new JLabel("5 Day Forecast");
 		lblDayFor.setBounds(308, 215, 102, 16);
 		frame.getContentPane().add(lblDayFor);
-		
+
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBounds(308, 243, 311, 182);
 		frame.getContentPane().add(scrollPane);
-		
+
 		JPanel panel = new JPanel();
 		scrollPane.setViewportView(panel);
-		
-		forecasts = new ForecastPanel[forecastDays+1];
-		for(int i = 0; i < forecasts.length; i++) {
+
+		forecasts = new ForecastPanel[forecastDays + 1];
+		for (int i = 0; i < forecasts.length; i++) {
 			forecasts[i] = new ForecastPanel("Day " + i);
 			forecasts[i].setSize(110, 110);
 			forecasts[i].setVisible(true);
 			panel.add(forecasts[i]);
 		}
-		
-		 frame.setVisible(true);
-		 
-	}
-	
 
-	
-	public JButton getGoButton(){
+		frame.setVisible(true);
+
+	}
+
+	/**
+	 *  Gets the go button
+	 * @return the go button
+	 */
+	public JButton getGoButton() {
 		return btnGo;
 	}
-	
-	public String getZipCode(){
+
+	public String getZipCode() {
 		return zipCodeField.getText();
 	}
 
 	/**
-	 * @param currentTempValue the currentTempValue to set
+	 * @param currentTempValue
+	 *            the currentTempValue to set
 	 */
 	public void setCurrentTempValue(String currentTempValue) {
 		this.currentTempValue.setText(currentTempValue);
 	}
 
 	/**
-	 * @param currentWindLabel the currentWindLabel to set
+	 * @param currentWindLabel
+	 *            the currentWindLabel to set
 	 */
 	public void setCurrentWindLabel(String currentWindLabel) {
 		this.currentWindLabel.setText(currentWindLabel);
 	}
 
 	/**
-	 * @param currentWindSpeedValue the currentWindSpeedValue to set
+	 * @param currentWindSpeedValue
+	 *            the currentWindSpeedValue to set
 	 */
 	public void setCurrentWindSpeedValue(String currentWindSpeedValue) {
 		this.currentWindSpeedValue.setText(currentWindSpeedValue);
 	}
 
-	
-
 	/**
-	 * @param d the currentHumdityValue to set
+	 * @param d
+	 *            the currentHumdityValue to set
 	 */
 	public void setCurrentHumdityValue(String d) {
 		this.currentHumdityValue.setText(d);
 	}
 
 	/**
-	 * @param currentPressureValue the currentPressureValue to set
+	 * @param currentPressureValue
+	 *            the currentPressureValue to set
 	 */
 	public void setCurrentPressureValue(String currentPressureValue) {
 		this.currentPressureValue.setText(currentPressureValue);
 	}
 
 	/**
-	 * @param currentObservation the currentObservation to set
+	 * @param currentObservation
+	 *            the currentObservation to set
 	 */
 	public void setCurrentObservation(String currentObservation) {
 		this.currentObservation.setText(currentObservation);
 	}
 
-    /**
-     *
-     * @param high
-     */
+	/**
+	 *
+	 * @param high
+	 */
 	public void addHighToChart(double high) {
 		chart.addHigh(high);
 	}
 
-    /**
-     *
-     * @param low
-     */
+	/**
+	 *
+	 * @param low
+	 */
 	public void addLowToChart(double low) {
 		chart.addLow(low);
 	}
 
+	/**
+	 * 
+	 * @param low
+	 * @param high
+	 * @param description
+	 * @param index
+	 */
 	public void setForecasts(String low, String high, String description, int index) {
-		forecasts[index-1].setHigh(high);
-		forecasts[index-1].setLow(low);
-		forecasts[index-1].setDescription(description);
+		forecasts[index - 1].setHigh(high);
+		forecasts[index - 1].setLow(low);
+		forecasts[index - 1].setDescription(description);
 	}
 	
-	public int getForecastDays(){
+	/**
+	 * 
+	 * @return
+	 */
+	public int getForecastDays() {
 		return forecastDays;
 	}
 
-	
 }
--- SwingingWeather/src/swingingWeather/TrendChart.java
+++ SwingingWeather/src/swingingWeather/TrendChart.java
@@ -8,75 +8,81 @@ import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 
 /**
- *
+ * This class is the trend chart
  */
 public class TrendChart {
-    private XYChart.Series highSeries;
-    private XYChart.Series lowSeries;
-    private LineChart<Number, Number> lineChart;
-    private int currentHigh=0;
-    private  int currentLow=0;
+	private XYChart.Series highSeries;
+	private XYChart.Series lowSeries;
+	private LineChart<Number, Number> lineChart;
+	private int currentHigh = 0;
+	private int currentLow = 0;
 
-    /**
-     *
-     */
-    public TrendChart() {
-        highSeries = new XYChart.Series();
-        lowSeries = new XYChart.Series();
-        highSeries.setName("High");
-        lowSeries.setName("Low");
-    }
+	/**
+	 * Constructor
+	 */
+	public TrendChart() {
+		highSeries = new XYChart.Series();
+		lowSeries = new XYChart.Series();
+		highSeries.setName("High");
+		lowSeries.setName("Low");
+	}
 
-    /**
-     *
-     * @return
-     */
-    private Scene getScene() {
-        NumberAxis xAxis = new NumberAxis();
-        NumberAxis yAxis = new NumberAxis();
-        this.lineChart = new LineChart<Number,Number>(xAxis,yAxis);
-        xAxis.setLabel("Day");
-        yAxis.setLabel("Temp");
-        lineChart.setTitle("High and Lows for next 7 days");
-        lineChart.getData().add(highSeries);
-        lineChart.getData().add(lowSeries);
-        return new Scene(lineChart,300,150);
+	/**
+	 * Create scene for the chart
+	 *
+	 * @return the scene with the chart
+	 */
+	private Scene getScene() {
+		NumberAxis xAxis = new NumberAxis();
+		NumberAxis yAxis = new NumberAxis();
+		this.lineChart = new LineChart<Number, Number>(xAxis, yAxis);
+		xAxis.setLabel("Day");
+		yAxis.setLabel("Temp");
+		lineChart.setTitle("High and Lows for next 7 days");
+		lineChart.getData().add(highSeries);
+		lineChart.getData().add(lowSeries);
+		return new Scene(lineChart, 300, 150);
 
-    }
+	}
 
-    /**
-     *
-     * @param fxPanel
-     */
-    public void initFX(JFXPanel fxPanel) {
-        Scene scene = getScene();
-        fxPanel.setScene(scene);
-    }
+	/**
+	 * This method creates the chart on the give JFXPanel
+	 *
+	 * @param fxPanel
+	 */
+	public void initFX(JFXPanel fxPanel) {
+		Scene scene = getScene();
+		fxPanel.setScene(scene);
+	}
 
-    /**
-     *
-     * @param high
-     */
-    public void addHigh(double high) {
-        highSeries.getData().add(new XYChart.Data(currentHigh, high));
-        currentHigh++;
-    }
+	/**
+	 * Adds a value to the high seris
+	 *
+	 * @param high
+	 */
+	public void addHigh(double high) {
+		highSeries.getData().add(new XYChart.Data(currentHigh, high));
+		currentHigh++;
+	}
 
-    /**
-     *
-     * @param high
-     */
-    public void addLow(double high) {
-        lowSeries.getData().add(new XYChart.Data(currentLow, high));
-        currentLow++;
-    }
+	/**
+	 * Adds a value to the low seris
+	 *
+	 * @param low
+	 */
+	public void addLow(double low) {
+		lowSeries.getData().add(new XYChart.Data(currentLow, low));
+		currentLow++;
+	}
 
-    public void clearChart() {
-    	 highSeries.getData().clear();
-    	 lowSeries.getData().clear();
-    	 currentLow = 0;
-    	 currentHigh = 0;
-    }
-    
+	/**
+	 * Clears all values from the chart
+	 */
+	public void clearChart() {
+		highSeries.getData().clear();
+		lowSeries.getData().clear();
+		currentLow = 0;
+		currentHigh = 0;
+	}
 
 }
